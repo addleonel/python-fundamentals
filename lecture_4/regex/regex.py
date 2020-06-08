df51d5c78a77a13984d12f918d85c7c6dcf085e2
leonel-123 <cambroni3633@gmail.com>
2020-06-08 12:30:39 -0500

    principals function of re module

--- lecture_4/regex/regex.py
+++ lecture_4/regex/regex.py
@@ -21,16 +21,93 @@ import re
 
 # FUNCTIONS      -- DESCRIPTION
 # =====================================================================
-# findall()      -- return a list all matches
-# search()       -- return a Match object if there is a math anywheter in the string
-# split()        -- return a list where the string has been split at each match
+# match()        -- returns the first occurrence
+# search()       -- returns a Match object if there is a math anywheter in the string
+# findall()      -- returns a list all matches
+# split()        -- returns a list where the string has been split at each match
 # sub()          -- replaces one or many matches with a string
 
+# using match function
+list_urls = [
+    'https://www.google.com',
+    'http://coreyms.com',
+    'https://youtube.com',
+    'https://www.nasa.gov',
+]
+pattern_url = r'(https)://(www.)?([\w]+)\.([\w]+)'
+for url in list_urls:
+    url_matched = re.match(pattern_url, url)
+    if url_matched:
+        print(url_matched.groups())  # returns all groups in a tuple
+        print(url_matched.group())   # returns the whole match
+        print(url_matched.group(0))  # returns the whole match, same of group()
+        print(url_matched.group(1))  # returns the first group
+        print(url_matched.group(2))  # returns the second group
+        print(url_matched.group(3))  # returns the third group
+        print(url_matched.group(4))
+        print(url_matched.start())  # where starts the match
+        print(url_matched.end())    # where ends the match
+        print(url_matched.span())   # returns a tuple (start, end)
+        print(url_matched.string, type(url_matched.string))   # returns a string
+        print('='*50)
+    else:
+        print('no match\n{}'.format('='*50))
+else:
+    print('proccess finished')
+# using search() function
 # Check if the string starts with "The" and ends with "Spain"
 s = 'The rain in Spain'
-pattern = '^The.*Spain$'
-r = re.search(pattern, s)
+pattern1 = r'^The.*Spain$'
+r = re.search(pattern1, s)
 if r:
     print("YES!")
 else:
     print("NO")
+
+list_emails = ['CoreyMSchafer@gmail.com',
+                'corey.schafer@university.edu',
+                'corey-321-schafer@my-work.net',
+                ]
+pattern_email = r'([a-zA-Z0-9_.+*-]+)@([a-zA-Z0-9_+*-]+)\.([a-zA-Z0-9_.+*-]+)'
+for email in list_emails:
+    email_matched = re.search(pattern_email, email)
+    if email_matched:
+        print(email_matched.groups())  # returns all groups in a tuple
+        print(email_matched.group())   # returns the whole match
+        print(email_matched.group(0))  # returns the whole match, same of group()
+        print(email_matched.group(1))  # returns the first group
+        print(email_matched.group(2))  # returns the second group
+        print(email_matched.group(3))  # returns the third group
+        print(email_matched.start())  # where starts the match
+        print(email_matched.end())    # where ends the match
+        print(email_matched.span())   # returns a tuple (start, end)
+        print(url_matched.string, type(url_matched.string))   # returns a string
+        print('{0}: {1} {2} {3} \t found a match\n'.format(email,email_matched.group(1),email_matched.group(2),email_matched.group(3)))
+    else:
+        print('{0}: {1} {2} {3} \t not match\n'.format(email,email_matched.group(1),email_matched.group(2),email_matched.group(3)))
+else:
+    print('proccess finished')
+# using findall function
+pattern2 = r'ai'
+matched = re.findall(pattern2, 'The rain in Spain') # returns a list
+print(matched)
+
+# using split function
+pattern3 = r'\s'
+splitted1 = re.split(pattern3, 'we are the champions') # returns a list splitted
+print(splitted1)
+splitted2 = re.split(pattern3, 'we need just one day to improve with regex', 3) # returns a list splitted at the third space
+print(splitted2)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt, 2)  # Split the string at the first white-space character:
+print(x)
+
+# using sub function
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2) #Replace the first two occurrences of a white-space character with the digit 9:
+print(x)
