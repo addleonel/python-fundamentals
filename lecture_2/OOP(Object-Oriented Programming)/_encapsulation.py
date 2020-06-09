88c01d8452de19582e44c0ed795f9f92dc003d4c
leonel-123 <cambroni3633@gmail.com>
2020-06-09 16:13:30 -0500

    property decorator

--- lecture_2/OOP(Object-Oriented Programming)/_encapsulation.py
+++ lecture_2/OOP(Object-Oriented Programming)/_encapsulation.py
@@ -1,4 +1,4 @@
-# METODOS Y TRIBUTOS DÉBILES PRIVADOS UTILIZAN "_" al inico de su nombre
+# METODOS Y ATRIBUTOS DÉBILES PRIVADOS UTILIZAN "_" al inico de su nombre
 # CONOCIDO COMO 'AMBITO PROTEGIDO'
 #
 #
@@ -6,7 +6,7 @@ import math
 
 class Client:
     _codigo = 2343
-    
+
 
 
 person = Client()
@@ -18,32 +18,32 @@ print(Client._codigo)
 # ---------------------------------------------------------------------
 
 class Convert:
-    
+
     # -----------------------------------------------
     # NECESITA DE UN OBJETO O INSTANCIAR (SE UTILIZA self)
     
     def __init__(self):
         self._to_radian = math.pi/180
         self._to_sexage = 180/math.pi
-        
+
     def convert_to_radian(self, value):
         value_converted = value*self._to_radian
         self._printvalue(value_converted)
-        
+
     def convert_to_sexage(self, value):
         value_converted = value*self._to_sexage
         self._printvalue(value_converted)
-    
+
     def _printvalue(self, value):
         print("Result = {}".format(value))
-    # ----------------------------------------------    
-        
+    # ----------------------------------------------
+
     # ----------------------------------------------
     # NO REQUIERE DE UN OBJETO O INSTANCIA (NO UTILIZA self)
-    
+
     def add(x, y):
         print(x + y)
-        
+
     # ----------------------------------------------
 
 num = Convert() # self es el valor de la instancia
@@ -56,4 +56,3 @@ print(num._to_sexage)
 
 # no necesita de un objeto
 Convert.add(2, 5)
-
