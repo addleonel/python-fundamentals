ed9048e6683f87fd873240ccd02031bb0f4ded21
A. D. Leonel <62483482+leonel-123@users.noreply.github.com>
2020-08-01 21:51:16 -0500

    data type added

--- lecture_1/data_type.py
+++ lecture_1/data_type.py
@@ -1,62 +1,62 @@
-#data type
+# data type
 
-#string
+# string
 print("My title")
-print("tipo de datos")
-type("this a string")#this is a string
+print("data type")
+type("this a string")  # this is a string
 
-#integer
+# integer
 print(100)
-tipo = type(100)#this is a integer
+tipo = type(100)  # this is a integer
 print(tipo);
 
-#float
+# float
 print(20.3)
-tipo_2 = type(20.3) #this is a float 
+tipo_2 = type(20.3)  # this is a float 
 print(tipo_2);
 
-#boolean
+# boolean
 True 
-type(True) #this is a boolean
+type(True)  # this is a boolean
 False 
-type(False) #this is a boolean
+type(False)  # this is a boolean
  
-#list
-    #it change
+# list
+# it change
 [12,12,35,56,24]
 ["time","watch","view","options"]
 [12,"book",True,20.3]
-    # other list
+# other list
 my_list= [12,23,34,45,56,56,45]
 print(my_list);
 
 
-#tuples
-    #it don't change
+# tuples
+# it don't change
 (12,23,34,565)
 ()
-    # other tuple
+# other tuple
 my_tuple = ("my name","my surname","my password")
 print(my_tuple)
 
-#dictionary
+# dictionary
 {
-    "name":"cam",
-    "surname":"Aquino",
+    "name":"Albert",
+    "surname":"Einstein",
     "age": 20
 }
 print({
-    "name":"cam",
-    "surname":"Aquino",
+    "name":"Albert",
+    "surname":"Einstein",
     "age": 20
 })
 print(type({
-    "name":"cam",
-    "surname":"Aquino",
+    "name":"Albert",
+    "surname":"Einstein",
     "age": 20
 }))
 
-    # other dictionary
+# other dictionary
 my_distionary = {"namebook":"Physics",
                 "authorbook":"Albert Einstein",
                 "editorialbook":"The cambrige"}
