85fe8e33b3ccf2ec245f5fdb44fd4806b0e99392
leonel-123 <cambroni3633@gmail.com>
2020-08-03 19:01:12 -0500

    functions in python

--- lecture_1/functions.py
+++ lecture_1/functions.py
@@ -1,18 +1,21 @@
-#using funcions
-def sumwithinput():
-    num1=int(input("first number: "))
-    num2=int(input("second number: "))
-    suma=num1+num2
-    return suma
+# using funcions
+def sumWithInput():
+    num1 = int(input("first number: "))
+    num2 = int(input("second number: "))
+    sum_ = num1 + num2
+    return sum_
+
+
 def printTheSum(number):
-    if number%2 ==0:
-        print("result : ",number)
+    if number % 2 == 0:
+        print("result : ", number)
         print("it's pair")
     else:
-        print("result : ",number)
+        print("result : ", number)
         print("it's odd")
 
-numero= sumwithinput()
-printTheSum(numero)
-print(type(sumwithinput()))
+
+number_ = sumWithInput()
+printTheSum(number_)
+print(type(sumWithInput()))
 
