1225c63ac9049f3eaeea2b933c1f6d14177ff51f
A. D. Leonel <62483482+leonel-123@users.noreply.github.com>
2020-08-05 17:07:07 -0500

    variables in Python

--- lecture_1/variable.py
+++ lecture_1/variable.py
@@ -1,19 +1,19 @@
-#using variable
+# using variable
 
-#string
+# string
 name = "cam"
 print(name)
 
-#integer 
+# integer 
 num_1 =212
 num_2 = 144
 result = num_1+num_2
 print(result)
 
-#list
+# list
 list_1 = [12,23,23,12]
 
-#together
+# together
 number, name = 12,"Tim"
 print(number)
 print(name)
