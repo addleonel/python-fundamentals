ee1b7ebbe3ba826440e8868d7d1a7acb08f2ed50
A. D. Leonel <62483482+leonel-123@users.noreply.github.com>
2020-08-07 17:10:47 -0500

    'for' keyword in Python

--- lecture_1/using_for.py
+++ lecture_1/using_for.py
@@ -1,22 +1,22 @@
-# Inthis section I am going to use the for 
+# In this section I am going to use the for 
 
 sum=0
-for i in range(1,11):
+for i in range(1, 11):
     sum += i
 
 print("the sum is: {0}".format(sum))
 
 # using 'for' 
 print ("other operation")
-for i in range(1,6):
-    for j in range(1,i+1):
+for i in range(1, 6):
+    for j in range(1, i+1):
         print(i)
     print()
 # using again
 print("other function")
 for i in range(1, 11):
-    if i%2==0:
-        for j in range(1,i+1):
+    if i % 2 == 0:
+        for j in range(1, i+1):
             print(i)
     else:
         print("it's odd")
