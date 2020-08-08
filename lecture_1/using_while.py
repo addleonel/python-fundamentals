28233c3be63f40637b35d534262aa2cbf26ff116
A. D. Leonel <62483482+leonel-123@users.noreply.github.com>
2020-08-08 18:49:38 -0500

    'while' in Python

--- lecture_1/using_while.py
+++ lecture_1/using_while.py
@@ -1,10 +1,10 @@
 # using while loop
-sum =0
-n=153
-while n>0:
+sum = 0
+n = 153
+while n > 0:
     print("value of n: {0}".format(n))
-    r=n%10
-    print("resto: {0}".format(r))
+    r = n % 10
+    print("modulus: {0}".format(r))
     sum += r
-    n=n//10
+    n = n//10
 print("the sum is: {0}".format(sum))    
