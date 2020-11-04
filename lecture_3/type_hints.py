cefdfff4626b149a6715ea9282f25e5138b3bbf0
addleonel <cambroni3633@gmail.com>
2020-11-04 12:28:36 -0500

    type hints from PEP 483/484

---
+++ lecture_3/type_hints.py
@@ -0,0 +1,24 @@
+# PEP 484 -- Type Hints
+# Type Definition Syntax
+
+
+def greeting(name: str = "Nothing") -> str:
+    """
+    The function receives a string an returns a string
+    """
+    msg: str = "Hello"
+    return msg + " " + name
+
+
+def sum_three_numbers(a: float, b: float, c: float) -> float:
+    """
+    This function receives three floats and return the sum
+    """
+    float_type = float  # type aliases
+    avg: float_type = (a + b + c)/3
+
+    return avg
+
+
+print(greeting())
+print(sum_three_numbers(23.3, 45.1, 9))
