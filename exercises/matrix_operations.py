ec117b6527abb4a0ab042f39c9a3dd80489d438b
addleonel <cambroni3633@gmail.com>
2021-01-06 18:43:54 -0500

    matrix Operations

--- exercises/matrix_operations.py
+++ exercises/matrix_operations.py
@@ -1,76 +1,93 @@
-def matrixSum():
-    # in this function I'm going to sum two matrix
-    print("SUM")
+
+# Matrix Operations
+
+
+def matrixAddition(m1, m2):
+    result = []
+    values = []
+    value = 0
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]): 
+        for i in range(len(m1)):
+            for j in range(len(m1[0])):
+                value = m1[i][j]+m2[i][j]
+                values.append(value)
+                value = 0
+            result.append(values)
+            values = []
+        return result
+    return "The matrices don't have correct dimensions"
+
+
+
+def matrixSubtraction(m1, m2):
+    result = []
+    values = []
+    value = 0
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]): 
+        for i in range(len(m1)):
+            for j in range(len(m1[0])):
+                value = m1[i][j]-m2[i][j]
+                values.append(value)
+                value = 0
+            result.append(values)
+            values = []
+        return result
+    return "The matrices don't have correct dimensions"
+
+def matrixMultiplication(m1, m2):
+    result = []
+    values = []
+    value = 0
+    if len(m1[0]) == len(m2):
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m1[0])):
+                    value += m1[i][k]*m2[k][j]
+                values.append(value)
+                value = 0
+            result.append(values)
+            values = []
+        return result
+    return "The matrices don't have correct dimensions"
+    
+                        
+
+
+def printLine():
+    print("-"*50)
+
+if __name__ == '__main__':
+    
     matrix_1 =[
         [2,1,2],
         [2,3,1],
         [1,0,2],
     ]
+
     matrix_2 = [
         [1,0,2],
         [1,4,2],
         [1,3,2],
     ]
-    for i in range(3):
-        for j in range(3):
-            matrix_result = matrix_1[i][j]+matrix_2[i][j]
-            print(matrix_result,end =" ") #For line print
-        print()
 
+    print(matrixAddition(matrix_1, matrix_2)) 
+    printLine()
+    print(matrixSubtraction(matrix_1, matrix_2))
+    printLine()
+    m1 = [
+        [1, 0, 2, 3],
+        [4, 5, 1, 2],
+        [1, 0, 1, 2],
+    ]
 
-
-def matrixRest():
-    print("REST")
-    matrix_1 = [
-        [1,2,3],
-        [1,0,3],
-        [1,2,0],
+    m2 = [
+        [0, 0, 2, 3],
+        [4, 5, 1, 6],
+        [1, 1, 2, 7],
+        [3, 0, 1, 9],
     ]
-    matrix_2 =[
-        [1,2,3],
-        [1,0,2],
-        [1,0,3],
-    ]
-    for i in range(3):
-        for j in range(3):
-            matrix_result = matrix_1[i][j]-matrix_2[i][j]
-            print(matrix_result, end =" ")
-        print()
-
-def matrixMultiply():
-    print("MULTIPLY")
-    matrix_1 = [
-        [1,2,3],
-        [1,0,3],
-        [1,2,0],
-    ]
-    matrix_2 =[
-        [1,2,3],
-        [1,0,2],
-        [1,0,3],
-    ]
-    result =[
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ]
-    
-    for i in range(3):
-        for j in range(3):
-            for k in range(3):
-                matrix_sum = matrix_1[i][k]*matrix_2[k][j]
-                result[i][j] += matrix_sum
-    for num in result:
-        print(num)
-def printLine():
-    print("------------------------")
-# matrix sum
-matrixSum() 
-printLine()
-matrixRest()
-printLine()
-matrixMultiply()
-    
+    print(matrixMultiplication(m1, m2))
+        
 
     
 
