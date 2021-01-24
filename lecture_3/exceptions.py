5a7fe3fc28cd54fabc906059e036b5b0f0bcaadb
addleonel <cambroni3633@gmail.com>
2021-01-24 18:56:00 -0500

    Exceptions

--- lecture_3/exceptions.py
+++ lecture_3/exceptions.py
@@ -1,135 +1,112 @@
-# MORE INFORMATION
-# https://docs.python.org/3/tutorial/errors.html
+# EXCEPTIONS
+# More info: https://docs.python.org/3/tutorial/errors.html
+# Advice: Only concentrate on the exceptions
 
-# class B(Exception):
-#   pass
-
-# class C(B):
-#   pass
-
-# class D(C):
-#   pass
-
-# for cl in [B, C, D]:
-#   try:
-#     raise cl
-#   except D:
-#     print("D")
-#   except B:
-#     print("B")
-#   except C:
-#     print("C")
-
-# try:
-#   raise NameError
-#   raise SyntaxError
-# except Exception:
-#   print("hello there is a error") 
-
-# for k in [NameError, ValueError, SyntaxError]:
-#   try:
-#     raise k
-#   except NameError:
-#     print("There is a NameError")
-#   except ValueError:
-#     print("There is a ValueError")
-#   except SyntaxError:
-#     print("There is a SyntaxError")
-
-
-# try:
-#     with open("myfile.txt", "r") as f:
-#       lines = f.readlines()
-#       for l in lines:
-#         print(l.strip('\n'))
-# except OSError as err:
-#     print("OS error: {0}".format(err))
-# except ValueError:
-#     print("Could not convert data to an integer.")
-# except:
-#     print("Unexpected error:", sys.exc_info()[0])
-#     raise
-
-# USING try ... except ... else ... finally
-
-for k in [1.0,"two", 3.0]:
+def sequence_of_the_exceptions():
+    """
+    If an exception occurs during the execution of the try clause,
+    the exception can be handled by an except clause.
+    If the except clause does not handle the exception,
+    the exception is rebuilt after the finally clause has been executed.
+    """
     try:
-        i = int(k)
-    except ValueError:
-        print("Error")
-    else:
-        print("yep")
+        raise NameError("Hi error")
+    except SyntaxError:
+        print("Error SyntaxError")
     finally:
-        print("completed process\n"+"="*30)
+        print("Finished")
 
 
-# If there is a finally clause present, the finally clause will run as the last task
-# before try the instruction complete. The finally clause is executed
-# regardless of whether or not the try statement throws an exception. The following
-# Points discuss more complex cases when an exception occurs:
+def behavior_with_break_continue_statements():
+    """
+    If the try statement reaches a break, continue or return communicated,
+    the finally clause will be executed just before the break,
+    continue return the declaration execution.
+    """
+    n = 0
+    while n < 10:
+        try:
+            print(n)
+            if n == 4:
+                break
+            print("Bye")
+        except Exception:
+            print("Error")
+        finally:
+            print("finished")
+        n += 1
 
-# 1- If an exception occurs during the execution of the try clause,
-#   the exception can be handled by an except clause.
-#   If the except clause does not handle the exception,
-#   the exception is rebuilt after the finally clause has been executed.
-# 2- An exception could occur during the execution of an except or the clause.
-#   Again, the exception is rebuilt after finally the clause has been executed.
 
-# 3- If the try statement reaches a break, continue or return communicated,
-#   the finally clause will be executed just before the break, continueo return the declaration execution.
+def behavior_of_return_in_try_finally():
+    """
+    If a finally clause includes a return declaration,
+    the returned value will be the one of
+    the declaration of the finally clause return,
+    not the value of the declaration of the try clause return.
+    """
+    try:
+        return True
+    finally:
+        return False
 
-# 4- If a finally clause includes a return declaration, the returned value will be the one of
-#   the declaration of the finally clause return, not the value of the declaration of the try clause return.
+# CORRECT USE OF try, except, else, finally keywords
+# syntax: try ... except ... else ... finally
 
-# 1-
-# try:
-#   raise NameError("Hi error")
-# except SyntaxError:
-#   print("error SyntaxError")
-# finally:
-#   print("FINISHED")
-# 3-
-# n = 0
-# while n < 10:
-#   try:
-#     print(n)
-#     if n == 4:
-#       break
-#     print("Bye")
-#   except Exception:
-#     print("Error")
-#   finally:
-#     print("finished")
-#   n += 1
-# 4-
-# def example():
-#   try:
-#     return True
-#   finally:
-#     return False
-# print(example()) # return False
+# example 1
+def through_list(list_):
+    """
+    Evaluate if is valid convert each values of the list
+    to integer
+    """
+    for k in list_:
+        try:
+            i = int(k)
+        except ValueError:
+            print("Error")
+        else:
+            print("Yep")
+        finally:
+            print("Completed process\n"+"="*30)
 
-# CORRECT USE of try ... except ... else ... finally ...
-
+# example 2
 def divide(x, y):
+    """
+    Evaluate if is valid divide 'x' value by 'y' value
+    """
     try:
         r = x/y
     except ZeroDivisionError:
-        print("Not divide by zero")
+        print('Not divide by zero')
     except TypeError:
-        print("Not input an string")
+        print('Not input an string')
     else:
-        print("the result is: {}".format(round(r, 2)))
+        print(f'The result is: {round(r, 2)}')
     finally:
-        print("completed process\n" + "="*30)
+        print('Completed process\n' + '='*30)
+
+# USING raise
+
+# example 1
+def concat_by(string_):
+    """
+    Concat string_ value. If this value is not a string
+    print and message error
+    """
+    if not isinstance(string_, str):
+        raise ValueError('Only strings')
+    return 'Your message is: ' + string_
 
 
-divide(2, 3)
-divide(2, 0)
-divide('2', '3')
-divide(10, 2)
-
-
-
+if __name__ == '__main__':
+    # sequence_of_the_exceptions()
+    # behavior_with_break_continue_statements()
+    through_list([1.0, 'two', 3])  # Yep Error Yep
+    divide(2, 3)  # the result is: 0.67
+    divide(2, 0)  # Not divide by zero
+    divide('2', '3')  # Not input an string
+    divide(10, 2)  # the result is: 5.0
+    print(concat_by('Through the eyes'))  # Your message is: Though the eyes
+    # print(concat_by(32))  # Only strings
+    print(behavior_of_return_in_try_finally())  # return False
 
 
