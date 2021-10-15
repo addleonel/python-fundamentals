108c3127ae5aaea8941b56fe8ed6523d93b9fabf
addleonel <adleonel3633@gmail.com>
2021-10-14 22:29:41 -0500

    snakecase

--- lecture_1/functions.py
+++ lecture_1/functions.py
@@ -1,12 +1,12 @@
-# using funcions
-def sumWithInput():
+# using functions
+def sum_with_input():
     num1 = int(input("first number: "))
     num2 = int(input("second number: "))
     sum_ = num1 + num2
     return sum_
 
 
-def printTheSum(number):
+def print_the_sum(number):
     if number % 2 == 0:
         print("result : ", number)
         print("it's pair")
@@ -15,7 +15,8 @@ def printTheSum(number):
         print("it's odd")
 
 
-number_ = sumWithInput()
-printTheSum(number_)
-print(type(sumWithInput()))
+if __name__ == "__main__":
+    number_ = sum_with_input()
+    print_the_sum(number_)
+    print(type(sum_with_input()))
 
