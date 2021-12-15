6b49cb494b62140efb0b272365e9a83dd80a381e
addleonel <adleonel3633@gmail.com>
2021-12-14 22:13:03 -0500

    more data types

--- lecture_1/data_type.py
+++ lecture_1/data_type.py
@@ -1,73 +1,57 @@
-# data type
+# Data Type
+if __name__ == '__main__':
 
-# string
-print("My title")
-print("data type")
-type("this a string")  # this is a string
+    # Text Type: string (str)
+    # string
+    print("My title")
+    print(type('this a string'))  # str, This is a string
 
-# integer
-print(100)
-tipo = type(100)  # this is a integer
-print(tipo);
+    # Numeric Types: integer (int), float, complex
+    # integer
+    print(100)
+    print(type(100))  # int, This is a integer
+    # float
+    print(20.3)
+    print(type(20.35))  # float, This is a float
+    # complex
+    print(4 + 3j)
+    print(type(3 + 4j))  # complex
 
-# float
-print(20.3)
-tipo_2 = type(20.3)  # this is a float 
-print(tipo_2);
-
-# boolean
-True 
-type(True)  # this is a boolean
-False 
-type(False)  # this is a boolean
+    # Boolean Type: bool
+    print(True)
+    print(type(False))  # bool, This is a boolean
  
-# list
-# it change
-[12,12,35,56,24]
-["time","watch","view","options"]
-[12,"book",True,20.3]
-# other list
-my_list= [12,23,34,45,56,56,45]
-print(my_list);
+    # Sequence Types: list, tuple, range
+    # list
+    print([12, 11, 35, 56, 24])
+    print(type([12, "book", True, 20.3]))  # list, This is a list
+    # tuples
+    print((12, 23, 34, 565))
+    print(type(()))  # tuple, This is a empty tuple
+    # range
+    print(range(20))
+    print(type(range(1, 10, 2)))  # range, This is range
 
+    # Mapping Type: dictionary (dict)
+    # dictionary
+    print({
+        "name": "Albert",
+        "surname": "Einstein",
+        "age": 20
+    })
+    print(type({
+        "name": "Albert",
+        "surname":  "Einstein",
+        "age": 20
+    }))  # dict, This is a dictionary
 
-# tuples
-# it don't change
-(12,23,34,565)
-()
-# other tuple
-my_tuple = ("my name","my surname","my password")
-print(my_tuple)
-
-# dictionary
-{
-    "name":"Albert",
-    "surname":"Einstein",
-    "age": 20
-}
-print({
-    "name":"Albert",
-    "surname":"Einstein",
-    "age": 20
-})
-print(type({
-    "name":"Albert",
-    "surname":"Einstein",
-    "age": 20
-}))
-
-# other dictionary
-my_distionary = {"namebook":"Physics",
-                "authorbook":"Albert Einstein",
-                "editorialbook":"The cambrige"}
-print(my_distionary)
-print()
-
-
-
-
-
-
+    # Set Types: set, frozenset (immutable set)
+    # set
+    print({4, 3, 2, 7})
+    print(type({56, 12, 34}))  # set, This is a set
+    # frozenset
+    print(frozenset([3, 4, 5, 6]))
+    print(type(frozenset({2, 2, 3, 3, 1})))  # frozenset, This is a frozenset
 
 
 
